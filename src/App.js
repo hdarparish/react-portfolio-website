51c4c8dcebc378a3508633fb3b8d0dcc05d513ad
hdarparish <60293987+hdarparish@users.noreply.github.com>
2020-06-22 10:24:27 -0400

    component setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import React from 'react';
-//import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+
 import './App.css';
-import Navigation from './components/navigation'
+import Home from './components/Home';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import Navigation from './components/Navigation';
 
-
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <div className="App">
+          <Navigation />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            <Route path="/about" component={About} />
+            <Route path="/contact" component={Contact} />
+            <Route component={Error} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,13 +1,17 @@
-import React from "react"
-import logo from '../assets/logo.svg';
+import React from 'react';
+ 
+import { NavLink } from 'react-router-dom';
 
+//import logo from '../assets/logo.svg';
+//import Contact from './components/Contact';
+/*
 class Navigation extends React.Component {
     render() {
         return (
             <div className="navbar">
                 <img className="logo" src={logo} alt ="Logo" />
                 <ul>
-                    <li><a href="#">Home</a></li>
+                    <li><a href="">Home</a></li>
                     <li><a href="#">Resume</a></li>
                     <li><a href="#">Projects</a></li>
                     <li><a href="#">Contact</a></li>
@@ -15,6 +19,18 @@ class Navigation extends React.Component {
             </div>
         )
     }
+}
+*/
+
+ 
+const Navigation = () => {
+    return (
+       <div className="navbar">
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+       </div>
+    );
 }
 
 export default Navigation;
